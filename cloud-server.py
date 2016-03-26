26bce3ed58507a1b83b5a09578b9fc28cd262459
dave c <dave.davecox@gmail.com>
2016-03-26 16:00:22 -0400

    still more updates

--- cloud-server.py
+++ cloud-server.py
@@ -12,15 +12,18 @@ import socket
 import mysql.connector
 import re
 import time
+import thread
+import select
 
 # defines:
 maxclouds = 10
 port = 8450
-host = socket.gethostname()
+host = "192.168.111.152"
+bufsize = 128
 maxconns = maxclouds
 debug = True
 
-def update_sql(unit_number, ip, color, last_contact):
+def update_sql(chipid, ip):
 	connargs = {
 		'user': 'root',
 		'password': 'toor',
@@ -30,14 +33,27 @@ def update_sql(unit_number, ip, color, last_contact):
 
 	cnx = mysql.connector.connect(**connargs)
 	cursor = cnx.cursor()
-
-	update_msg = ("UPDATE clouds SET ip='" + ip + "', color='" + color + "', last_contact='" + str(last_contact) + "' WHERE id='" + str(unit_number) + "'")
-
-	print update_msg
 	
-	cursor.execute(update_msg)
-	cnx.commit()	
+	chipids = ("SELECT * FROM clouds WHERE id='" + str(chipid) + "'") 
+	cursor.execute(chipids)
+
+	found = 0
+	for iterable in cursor:
+		for item in iterable:
+			if str(item) == str(chipid):
+				if debug:
+					print "ID MATCHES!: " + chipid
+				found = 1
+
+	if found == 0:
+		update = ("INSERT INTO clouds (id, ip, color, default_color, last_contact) VALUES ('" + str(chipid) + "', '" + str(ip) + "', '255,255,255', '255,255,255',  '" + str(int(time.time())) + "')")
+		cursor.execute(update)
+		cnx.commit()
+
 	cnx.close()
+
+	retval = True
+	return retval
 
 def write_log(string, error_level):
 	if error_level == 0:
@@ -53,53 +69,101 @@ def write_log(string, error_level):
 	print error_string + string
 	return 0	
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def set_all_colors(color):
+	connargs = {
+		'user': 'root',
+		'password': 'toor',
+		'host': '127.0.0.1',
+		'database': 'glow_cloud'
+	}
 
+	cnx = mysql.connector.connect(**connargs)
+	cursor = cnx.cursor()
+
+	update_colors = ("UPDATE clouds SET color='" + color + "'")
+	cursor.execute(update_colors)
+
+	# error checking here..
+	cnx.commit()
+	cnx.close()
+
+	return 1
+
+def reset_all_colors():
+	pass
+
+def get_color_by_id(chipid):
+	connargs = {
+		'user': 'root',
+		'password': 'toor',
+		'host': '127.0.0.1',
+		'database': 'glow_cloud'
+	}
+
+	cnx = mysql.connector.connect(**connargs)
+	cursor = cnx.cursor()
+
+	get_color = ("SELECT color FROM clouds WHERE id='" + chipid + "'")
+	cursor.execute(get_color)
+	
+	for color in cursor:
+		for value in color:
+			retval = value 
+
+	cnx.close()
+
+	return retval
+	
+def handler(conn, addr):
+	conn.setblocking(0)
+	x = 0 
+	color_delay = 0
+	while 1:
+		if x >= 30:
+			# conn.send("ledclr(" + from_sql[1] + ")")
+			conn.send("node.chipid()")
+			x = 0
+		
+		x += 1
+		time.sleep(0.5)
+
+
+		try:
+			data = conn.recv(bufsize)
+			print data
+
+			if data.endswith("BUTTON"):
+				newcolor = get_color_by_id(data.split(":")[0])		
+				print newcolor
+				set_all_colors(newcolor)
+				conn.send("ledclr(" + newcolor + ")")
+			elif len(data) == 7:
+				update_sql(data, addr[0])
+
+
+		except socket.error:
+			pass
+
+		# if data == update, store info in table
+		# if data == button, do a set_all_colors. +1 to button push counter as well
+		# if current_time = 2am, do a reset!
+			
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind((host, port))
-serversocket.listen(maxconns)
+serversocket.listen(maxclouds)
 
 while 1:
-	# wait for connections:
+	write_log("Server OK, waiting for connections on " + str(port) + ".", 0)
 	conn, addr = serversocket.accept()
-	pl = conn.recv(128)
-	if debug == True:
-		print pl
-
-	# validate sent data:
-	pl = pl.split("/")
 	
-	if int(pl[0]) < maxclouds:
-		unit_number = pl[0]
-		
-		if re.match('([0-9]{1,3}\.){3}[0-9]{1,3}', pl[1]):
-			ip = pl[1]
+	write_log("New client connected from: " + str(addr), 0)
+	thread.start_new_thread(handler, (conn, addr))
 
-			if re.match('^([0-9]{1,3},){2}[0-9]{1,3}$', pl[2]):
-				color = pl[2]
-
-				# if we're in here, we've validated the data. update the mysql table:
-				update_time = int(time.time())
-
-				from_sql = update_sql(unit_number, ip, color, update_time)
-
-			else:
-				write_log("Malformed color value sent.", 2)
-				if debug:
-					print pl[2]
-
-		else:
-			write_log("Malformed ip address value sent.", 2)
-			if debug:
-				print pl[1]
-
-	else:
-		write_log("Incorrect unit_number sent.", 2)
-		if debug:
-			print pl[0]
-	
-	# check what the color string should be:
-	
-	current_color = "255,25,0"
-
-	conn.send(current_color)
-
+# alright, so, final things required to make this shit work:
+# > SSL or encryption on messages between client and server.
+#	> will need to compile this into the nodemcu image.
+# > need to detect button sends correctly
+# > need to send updates correctly
+# > need to add node id as a column (instead of IP?) in table
+# > also need to keep track of # of button presses per cloud.
+# > nightly reboots when it's 2am or other arbitrary time (set by sql query?)
--- cloud.lua
+++ cloud.lua
@@ -1,13 +1,63 @@
 -- cloud.lua --
+-- called by init.lua. --
+local led = 4 -- GPIO2
+local button = 3 -- GPIO0
+server = "192.168.111.152"
+lastContact = 0
 
-local pin = 4 -- GPIO2
-local value = gpio.LOW
+gpio.mode(led, gpio.OUTPUT)
+gpio.mode(button, gpio.INT, gpio.PULLUP)
 
-gpio.mode(pin, gpio.OUTPUT)
+function debounce(func)
+	local last = 0
+	local delay = 200000
 
-while 1 do
-	for val = 0, 75, 1 do
-		tmr.delay(100)
-		ws2812.writergb(pin, string.char(val, 0, val*3))
+	return function(...)
+		local now = tmr.now()
+		if now - last < delay then return end
+
+		last = now
+		return func(...)
 	end
 end
+
+function onChange()
+	if gpio.read(button) == 0 then
+		sock:send(node.chipid()..":BUTTON")
+	end
+end
+
+sock = net.createConnection(net.TCP, 0)
+
+sock:on("connection", function(sck)
+	sck:send(node.chipid()..":"..wifi.sta.getip())
+	lastContact = 0
+end)
+
+sock:on("receive", function(sck, c)
+	node.input(c .. "\n")
+	lastContact = 0
+	sock:send(node.chipid())
+	print(c)
+end)
+
+function connect()
+	sock:connect(8450,server)
+end
+
+function ledclr(r, g, b)
+	ws2812.write(led, string.char(g, r, b, g, r, b, g, r, b))
+end
+
+gpio.trig(button, "down", debounce(onChange))
+
+tmr.alarm(6, 1000, tmr.ALARM_AUTO, function()
+	if lastContact > 60 then
+		sock:close()
+		connect()
+	else
+		lastContact = lastContact + 1
+	end
+end)
+
+connect()
